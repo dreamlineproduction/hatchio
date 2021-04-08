a6d6335ec69e34647ebb8c046ba2f8d2e62bda8f
dreamlineproduction <info@dreamlineproduction.in>
2021-04-08 17:32:06 +0530

    Working on international phone number inpuit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,26 @@
 var input = document.querySelector("#phone");
 window.intlTelInput(input, {
-    // any initialisation options go here
+
+
+
+    allowExtensions: true,
+    formatOnDisplay: true,
+    autoFormat: true,
+    autoHideDialCode: true,
+    autoPlaceholder: true,
+    defaultCountry: "auto",
+    ipinfoToken: "yolo",
+
+    nationalMode: false,
+    numberType: "MOBILE",
+
+    preventInvalidNumbers: true,
+    separateDialCode: true,
+    initialCountry: "auto",
+    geoIpLookup: function(callback) {
+        $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            callback(countryCode);
+        });
+    },
 });
